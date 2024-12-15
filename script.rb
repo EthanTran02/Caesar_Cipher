f34502650f3dd99bc3fd639d9a187cb633a2d15b
Ethan <khoitranec@gmail.com>
2024-12-15 21:39:19 +0700

    finish

---
+++ script.rb
@@ -0,0 +1,17 @@
+def caesar_cipher(string, shift)
+  shifted_string = string.downcase.chars.map do |char|
+    alphabet = ('a'..'z').to_a
+    if alphabet.include?(char)
+            old_index = alphabet.index(char)
+            new_index = (old_index + shift) % alphabet.size
+            alphabet[new_index]
+    else
+      char
+    end
+  end
+  shifted_string.join.capitalize
+end
+
+
+
+p caesar_cipher('What a string!', 5)
