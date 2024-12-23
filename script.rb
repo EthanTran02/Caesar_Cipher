7ae5a81e0b5a295c49a17173c77660c0fe168a63
Ethan <khoitranec@gmail.com>
2024-12-23 22:27:17 +0700

    Fixed with Robucop

--- script.rb
+++ script.rb
@@ -2,9 +2,9 @@ def caesar_cipher(string, shift)
   shifted_string = string.downcase.chars.map do |char|
     alphabet = ('a'..'z').to_a
     if alphabet.include?(char)
-            old_index = alphabet.index(char)
-            new_index = (old_index + shift) % alphabet.size
-            alphabet[new_index]
+      old_index = alphabet.index(char)
+      new_index = (old_index + shift) % alphabet.size
+      alphabet[new_index]
     else
       char
     end
